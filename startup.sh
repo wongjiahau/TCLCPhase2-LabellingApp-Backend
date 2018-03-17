01d3e88bc7f804b40893c1f444fd2eaec79d5a84
Wong Jia Hau <hou32hou@gmail.com>
2018-03-17 18:44:29 +0800

    Modified startup.sh to include code for importing json

--- startup.sh
+++ startup.sh
@@ -29,5 +29,12 @@ nodejs app.js &
 # Redirect port 3000 to port 80, because nodejs is running on port 3000
 sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
 
+#Import json files
+cd ~ 
+curl https://raw.githubusercontent.com/fesvictor/TCLCPhase2/master/analysis/transform_format_for_mongodb/english.json > english.json
+curl https://raw.githubusercontent.com/fesvictor/TCLCPhase2/master/analysis/transform_format_for_mongodb/chinese.json > chinese.json
+mongoimport --db tclc --collection english --drop --file ~/english.json --jsonArray
+mongoimport --db tclc --collection chinese --drop --file ~/chinese.json --jsonArray
+
 # REMEMBER to test the site using http instead of https!
 
