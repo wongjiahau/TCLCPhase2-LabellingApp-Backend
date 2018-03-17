56d056ec68d76a325a06b1a06eb0ef03d1911304
Wong Jia Hau <hou32hou@gmail.com>
2018-03-17 17:50:43 +0800

    Update

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.post('/login', (req, res) => {
+    const userId = req.body.userId;
+    const pass = req.body.pass;
+    res.setHeader('Content-Type', 'application/json');
+    const userIdIsCorrect = userId === 'some hashed userId';
+    const passwordIsCorrect = pass === 'some hashed password';
+    res.send(JSON.stringify({isAuthenticated: userIdIsCorrect && passwordIsCorrect}));
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
