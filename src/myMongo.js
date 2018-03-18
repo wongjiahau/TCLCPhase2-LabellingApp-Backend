170bd7ebd6fabc9b8df4c8c45b8c082f9f36e208
Wong Jia Hau <hou32hou@gmail.com>
2018-03-18 11:51:03 +0800

    Update

--- src/myMongo.js
+++ src/myMongo.js
@@ -1,11 +1,9 @@
-// @ts-check
 const MongoClient = require('mongodb').MongoClient;
 const URL = 'mongodb://localhost:27017';
 function MyMongo(dbName) {
     this.dbName = dbName;
 
     this.getSomeObjectIds = (callback) => {
-        callback(null, [1,2,3,4,5]);
         MongoClient.connect(URL, (err, client) => {
             const collection = client
                 .db(this.dbName)
