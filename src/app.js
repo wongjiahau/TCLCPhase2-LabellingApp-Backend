7b9f869d7b6ddb754f0c1da0b779fb76262aa5c6
Wong Jia Hau <hou32hou@gmail.com>
2018-04-01 23:19:52 +0800

    Fix some error for determining env variables

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const createApp = require("./createApp");
-const DBNAME = process.env.DEPLOY ? 'tclc' : 'test';
+console.log(process.env.DEPLOY);
+const DBNAME = process.env.DEPLOY === "1" ? 'tclc' : 'test';
 console.log("dbname is" + DBNAME);
 createApp(3000, DBNAME);
