cf0671610fd7e84709fdeb56d3217c9e3565bdba
Wong Jia Hau <hou32hou@gmail.com>
2018-04-01 23:07:29 +0800

    Updated init-containers.sh

--- init-containers.sh
+++ init-containers.sh
@@ -4,3 +4,5 @@ docker-compose build
 docker-compose up -d
 docker exec mymongo mongoimport --db test --collection english --drop --file /usr/data/english_sample.json --jsonArray
 docker exec mymongo mongoimport --db test --collection chinese --drop --file /usr/data/chinese_sample.json --jsonArray
+docker exec mymongo mongoimport --db tclc --collection english --drop --file /usr/data/english.json --jsonArray
+docker exec mymongo mongoimport --db tclc --collection chinese --drop --file /usr/data/chinese.json --jsonArray
