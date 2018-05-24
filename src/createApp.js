30ba6f1d5a4069c02ce83d2a92a09f2e8ee40173
Wong Jia Hau <hou32hou@gmail.com>
2018-05-24 16:44:53 +0800

    Moving useless files to deprecated folder

--- src/createApp.js
+++ src/createApp.js
@@ -86,48 +86,41 @@ function createApp(portNumber, useSampleData = false) {
     });
 
     app.post('/submitChinese', (req, res) => {
-        myMongo.submitUpdates('chinese', req.body, 
-            () => {res.send('success');},
-            (error) => {res.send('failed due to: ' + error)}
-        );
+        const result = tryDo(() => myMongo.submitUpdates("chinese", req.body));
+        if(result.error) {
+            res.send("Failed due to: " + result.error)
+            return;
+        }
+        res.send("ok");
     });
 
     app.get('/fetchAdminDataEnglish', (req, res) => {
-        myMongo.fetchAdminData('english', (err, item) => {
-            if(err) {
-                res.send(err);
-            }
-            res.setHeader('Content-Type', 'application/json');
-            res.send(item);
-        });
+        res.send("Unimplemented yet");
+        // myMongo.fetchAdminData('english', (err, item) => {
+        //     if(err) {
+        //         res.send(err);
+        //     }
+        //     res.setHeader('Content-Type', 'application/json');
+        //     res.send(item);
+        // });
     });
 
     app.get('/fetchAdminDataChinese', (req, res) => {
-        myMongo.fetchAdminData('chinese', (err, item) => {
-            res.setHeader('Content-Type', 'application/json');
-            res.send(item);
-        });
+        res.send("Unimplemented yet")
+        // myMongo.fetchAdminData('chinese', (err, item) => {
+        //     res.setHeader('Content-Type', 'application/json');
+        //     res.send(item);
+        // });
     });
 
     app.get('/fetchNumberOfPostLabelledToday', (req, res) => {
-        myMongo.fetchNumberOfPostLabelledToday('english', (err1, res1) => {
-            myMongo.fetchNumberOfPostLabelledToday('chinese', (err2, res2) => {
-                res.setHeader('Content-Type', 'application/json');
-                res.send(JSON.stringify(res1 + res2));
-            });
-        });
-    });
-
-
-    app.post('/login', (req, res) => {
-        const userId = req.body.userId;
-        const pass = req.body.pass;
-        res.setHeader('Content-Type', 'application/json');
-        const userIdIsCorrect = userId === 'some hashed userId';
-        const passwordIsCorrect = pass === 'some hashed password';
-        res.send(JSON.stringify({
-            isAuthenticated: userIdIsCorrect && passwordIsCorrect
-        }));
+        res.send("Unimplemented yet")
+        // myMongo.fetchNumberOfPostLabelledToday('english', (err1, res1) => {
+        //     myMongo.fetchNumberOfPostLabelledToday('chinese', (err2, res2) => {
+        //         res.setHeader('Content-Type', 'application/json');
+        //         res.send(JSON.stringify(res1 + res2));
+        //     });
+        // });
     });
 
     app.listen(portNumber, () => console.log(`The server is listening on port ${portNumber}!`));
